11f11a894f17bed1e56e6c6fda38e3a119bb09f1
GoranTelerik <gorancvetkov_sr@yahoo.com>
2015-11-07 15:52:06 +0200

    Web api skeleton architecture added! Closes #2

---
+++ Source/Data/Startkicker.Data/StartkickerDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Startkicker.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Startkicker.Data.Models;
+
+    public class StartkickerDbContext : IdentityDbContext<User>, IStartkickerDbContext
+    {
+        public StartkickerDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static StartkickerDbContext Create()
+        {
+            return new StartkickerDbContext();
+        }
+    }
+}
---
+++ Source/Server/Startkicker.Api/App_Start/DatabaseConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace Startkicker.Api
+{
+    using System.Data.Entity;
+    using Startkicker.Data;
+    using Startkicker.Data.Migrations;
+
+    public static class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<StartkickerDbContext, Configuration>());
+            StartkickerDbContext.Create().Database.Initialize(true);
+        }
+    }
+}
