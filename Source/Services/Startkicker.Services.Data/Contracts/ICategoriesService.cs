64b892b492c15aa7bde89095fa62700badf2246b
alexizvely <alexizvely@gmail.com>
2015-11-15 15:39:19 +0200

    Categories

---
+++ Source/Services/Startkicker.Services.Data/Contracts/ICategoriesService.cs
@@ -0,0 +1,14 @@
+﻿namespace Startkicker.Services.Data.Contracts
+{
+    using System.Linq;
+    using Startkicker.Data.Models;
+
+    public interface ICategoriesService
+    {
+        int Add(Category category);
+        IQueryable<Category> GetAll(int page = 1, int pageSize = 10);
+        Category GetById(int id);
+        void Remove(Category Category);
+        int Update(Category category);
+    }
+}
