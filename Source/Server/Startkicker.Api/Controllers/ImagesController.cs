5561b6890a7c073c60b538c50d65feaeacb7c6a1
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-11-15 17:34:45 +0200

    Adding Images controller

---
+++ Source/Server/Startkicker.Api/Controllers/ImagesController.cs
@@ -0,0 +1,39 @@
+﻿namespace Startkicker.Api.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    using Startkicker.Api.Models.Request.Images;
+    using Startkicker.Data.Models;
+    using Startkicker.Services.Data.Contracts;
+
+    public class ImagesController : ApiController
+    {
+        private readonly IImagesService images;
+
+        public ImagesController(IImagesService images)
+        {
+            this.images = images;
+        }
+
+        [HttpPost]
+        //[Authorize]
+        public async Task<IHttpActionResult> Add(NewImagesRequestModel request)
+        {
+            string imageUrl = await images.UploadAsync(request.ImageStream);
+            this.images.Add(
+                new Image
+                {
+                    ImageUrl = imageUrl,
+                    ProjectId = request.ProjectId,
+                    IsRemoved = false
+                });
+
+            return this.Ok(imageUrl);
+        }
+    }
+}
